89f84feb0618d75dff93f81e50b5f988620224be
Jennifer Swartz <swartzjenn@gmail.com>
2016-06-27 05:16:47 -0400

    Add item error type working

--- lib/errors.rb
+++ lib/errors.rb
@@ -1,3 +1,4 @@
 module UdaciListErrors
-  # Error classes go here
+  class InvalidItemType < StandardError
+  end
 end
--- lib/listable.rb
+++ lib/listable.rb
@@ -3,11 +3,20 @@ module Listable
     "#{description}".ljust(25)
   end
 
-  def format_date(start_date, end_date = nil)
+  def format_date(start_date, end_date=nil)
     dates = start_date.strftime("%D") if start_date
     dates << " -- " + end_date.strftime("%D") if end_date
     dates = "N/A" if !dates
     return dates
-  end      
+  end
+  
+  def format_priority(priority)
+    value = " ⇧" if priority == "high"
+    value = " ⇨" if priority == "medium"
+    value = " ⇩" if priority == "low"
+    value = "" if !priority
+    return value
+  end
 
+        
 end
